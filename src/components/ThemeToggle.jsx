a95434e132564a78d314eb5b37cc5fe212b9807f
Paramartha Ray <121241803+ParamarthaRay@users.noreply.github.com>
2025-03-30 13:05:13 +0530

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,8 +5,7 @@ const ThemeToggle = ({ darkMode, setDarkMode }) => {
       onClick={() => setDarkMode(!darkMode)}
       className={`fixed top-4 right-4 flex items-center gap-2 px-4 py-2 rounded-lg shadow-md font-semibold transition-all duration-300
                   ${darkMode ? "bg-orange-600 text-black" : "bg-blue-400 text-white"}
-                  hover:scale-105 transform`}
-    >
+                  hover:scale-105 transform`} >
       {darkMode ? <WiDaySunny className="text-yellow-400 text-2xl" /> : <WiMoonAltWaningCrescent5 className="text-gray-300 text-2xl" />}
       {darkMode ? "Light Mode" : "Dark Mode"}
     </button>
